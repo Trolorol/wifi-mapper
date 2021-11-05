e69bcc509e68de412ac173f55393fcea982a1a81
João Calapez <joao.calapez.c@gmail.com>
2021-11-05 15:47:29 +0000

    Testing routes

--- routes/test.js
+++ routes/test.js
@@ -6,36 +6,94 @@ const morgan = require('morgan');
 const _ = require('lodash');
 
 
+// ---------------
+const http = require('http');
+const fs = require('fs');
+const multer = require('multer');
+const csv = require('fast-csv');
+
+const upload = multer({ dest: 'tmp/csv/' });
+
 var router = express.Router();
 
-router.post('/upload-avatar', async (req, res) => {
+// const fileRows = [];
+// csv.fromPath(req.file.path)
+//     .on("data", function(data) {
+//         fileRows.push(data); // push each row
+//     })
+//     .on("end", function() {
+//         console.log(fileRows) //contains array of arrays. Each inner array represents row of the csv file, with each element of it a column
+//         fs.unlinkSync(req.file.path); // remove temp file
+//         //process "fileRows" and respond
+//     })
+
+router.post('/', upload.single('file'), async function(req, res) {
     try {
-        if(!req.files) {
+        if (!req.files) {
             res.send({
                 status: false,
                 message: 'No file uploaded'
             });
         } else {
-            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-            let avatar = req.files.avatar;
-            //Use the mv() method to place the file in upload directory (i.e. "uploads")
-            avatar.mv('./uploads/' + avatar.name);
-
-            //send response
-            res.send({
-                status: true,
-                message: 'File is uploaded',
-                data: {
-                    name: avatar.name,
-                    mimetype: avatar.mimetype,
-                    size: avatar.size
-                }
-            });
+            let file = req.files.file;
+            file.mv('./uploads/' + file.name);
+            let file_location = './uploads/' + file.name
+            const process = await file_processing(file_location);
         }
+        res.send({
+            status: true,
+            message: 'File is uploaded',
+            data: {
+                process
+            }
+        });
     } catch (err) {
         res.status(500).send(err);
     }
+
 });
 
+
+function file_processing(file) {
+    let promise = new Promise(function(resolve, reject) {
+
+        setTimeout(() => resolve("done!"), 1000);
+    });
+
+    // resolve runs the first function in .then
+    promise.then(
+        result => alert(result), // shows "done!" after 1 second
+        error => alert(error) // doesn't run
+    )
+};
+
+
+
+
+
+// router.post('/', async(req, res) => {
+//     try {
+//         if (!req.files) {
+//             res.send({
+//                 status: false,
+//                 message: 'No file uploaded'
+//             });
+//         } else {
+//             let file = req.files.file;
+//             file.mv('./uploads/' + file.name);
+//             res.send({
+//                 status: true,
+//                 message: 'File is uploaded',
+//                 data: {
+//                     name: file.name,
+//                     mimetype: file.mimetype,
+//                     size: file.size
+//                 }
+//             });
+//         }
+//     } catch (err) {
+//         res.status(500).send(err);
+//     }
+// });
 
 module.exports = router;
