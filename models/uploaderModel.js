8c569402da668373ec3686e10f41a3386d568cbb
João Calapez <joao.calapez.c@gmail.com>
2021-11-21 18:51:47 +0000

    Uploader working

--- models/uploaderModel.js
+++ models/uploaderModel.js
@@ -11,23 +11,78 @@ module.exports.uploadFile = async function(file) {
         .on("data", function(data) {
             arr.push(data);
         })
-        .on("end", async function() {
+        .on("end", async function() { // Se houver tempo fazer verificação de pontos repetidos na introdução do ficheiro
             try {
-                let sql_waps = "Insert into waps(bssid, strength, location) values($1,$2,$3)";
-                encryptions = await getEncryptions() //Lista de encryptions existentes
-                console.log(encryptions.result)
-                for (var i = 0; i < arr.length; i++) {
-                    // Pega na coluna ecnryption e faz lop
-                    // Se já existir pega no id e completa tabela intermedia
-                    // Se não existir cria um novo registo e volta ao passo anterior
-                    //await pool.query(sql, [row]); //não apagar
+                let encryptions = await getEncryptions();
+                for (element in arr) {
+                    let element_encryption = arr[element].Security;
+                    let get_encryption = await get_encrtpyion_by_name(element_encryption);
+                    let encryption_name = get_encryption.result.name
+                    let encryption_id = get_encryption.result.id
+                    if (encryption_name === element_encryption) {
+                        await insert_waps(arr[element].BSSID, arr[element].Strength, arr[element].Location, encryption_id)
+                    } else {
+                        let insert_encryption = await insert_encryptions(element_encryption);
+                        await insert_waps(arr[element].BSSID, arr[element].Strength, arr[element].Location, insert_encryption.result);
+                    }
                 }
                 return { status: 200 };
-            } catch (err) {
-                console.log(err);
-                return { status: 500, result: err };
+            } catch (error) {
+                console.log(error);
+                return { status: 500, result: error };
             }
         });
+}
+
+get_encrtpyion_by_name = async function(encryptionName) {
+    try {
+        let sql = "Select * from encryptions where encryption = $1"; // meter ilike
+        let result = await pool.query(sql, [encryptionName]);
+        let encryptions = result.rows[0];
+        if (typeof encryptions !== "undefined") {
+            return { status: 200, result: { id: encryptions.id, name: encryptions.encryption } };
+        } else {
+            return { status: 200, result: { id: null, name: null } };
+        }
+    } catch (error) {
+        console.log(error);
+        return { status: 500, result: error };
+    }
+}
+
+insert_encryptions = async function(encryptionName) {
+    try {
+        let sql = "Insert into encryptions(encryption) values($1) RETURNING id";
+        let encryption_id = await pool.query(sql, [encryptionName]);
+        return { status: 200, result: encryption_id.rows[0].id };
+    } catch (error) {
+        console.log(error);
+        return { status: 500, result: error };
+    }
+
+}
+
+insert_waps = async function(bssid, strenght, location, encryptionId) {
+    try {
+        let sql = "Insert into waps(bssid, strength, location) values($1,$2,$3) RETURNING id";
+        let wapId = await pool.query(sql, [bssid, strenght, location]);
+        encryptions = await insert_waps_encryptions(wapId.rows[0].id, encryptionId) //Falta return?
+        return { status: 200 };
+    } catch (error) {
+        console.log(error);
+        return { status: 500, result: error };
+    }
+}
+
+insert_waps_encryptions = async function(wapId, encryptionId) {
+    try {
+        let sql = "Insert into waps_encryptions(wap_id, encryption_id) values($1,$2)";
+        let result = await pool.query(sql, [wapId, encryptionId]); //Falta return?
+        return { status: 200, result: result };
+    } catch (error) {
+        console.log(error);
+        return { status: 500, result: error };
+    }
 }
 
 getEncryptions = async function() {
@@ -36,8 +91,8 @@ getEncryptions = async function() {
         let result = await pool.query(sql);
         let encryptions = result.rows;
         return { status: 200, result: encryptions };
-    } catch (err) {
-        console.log(err);
-        return { status: 500, result: err };
+    } catch (error) {
+        console.log(error);
+        return { status: 500, result: error };
     }
 }
--- routes/fileUploadRoutes.js
+++ routes/fileUploadRoutes.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const _ = require('lodash');
-const fs = require('fs');
 const multer = require('multer');
-const csvParser = require('csv-parser');
 const upload = multer({ dest: 'tmp/csv/' });
 const uploaderModel = require('../models/uploaderModel');
 const { result } = require('lodash');
@@ -11,7 +8,6 @@ var router = express.Router();
 
 
 router.post('/', upload.single('file'), async function(req, res) {
-    let process;
     try {
         if (!req.files) {
             res.send({
@@ -22,10 +18,7 @@ router.post('/', upload.single('file'), async function(req, res) {
             let file = req.files.file;
             file.mv('./uploads/' + file.name);
             let file_location = './uploads/' + file.name
-            console.log("Here")
-            console.log(file_location)
             await uploaderModel.uploadFile(file_location);
-            //process = await file_processing(file_location);
         }
         res.send({
             status: true,
@@ -35,8 +28,5 @@ router.post('/', upload.single('file'), async function(req, res) {
     } catch (err) {
         res.status(500).send(err);
     }
-
 });
-
-
 module.exports = router;
