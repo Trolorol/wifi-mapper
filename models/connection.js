abfff1f198afc71efa2b2afbab1a197efd138f75
João Calapez <joao.calapez.c@gmail.com>
2021-12-02 20:28:13 +0000

    Added env var to connection

--- models/connection.js
+++ models/connection.js
@@ -1,10 +1,22 @@
-var pg = require('pg')
-
-const connectionString = "postgres://admin:postgres@localhost:2345/wifi-mapper"
-const Pool = pg.Pool
-const pool = new Pool({
-    connectionString,
-    max: 10,
+const { Pool } = require('pg')
+    // pools will use environment variables
+    // for connection information
+const pool = new Pool()
+pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    pool.end()
 })
+
+
+
+
+// var pg = require('pg')
+
+// const connectionString = "postgres://admin:postgres@localhost:2345/wifi-mapper"
+// const Pool = pg.Pool
+// const pool = new Pool({
+//     connectionString,
+//     max: 10,
+// })
 
 module.exports = pool;
